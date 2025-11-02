4fae48d8510cedb2ce35aad834ef1440e5868202
AncientSphnx <inuman739@gmail.com>
2025-11-02 15:39:31 +0530

    done

--- frontend/src/components/ui/textarea.tsx
+++ frontend/src/components/ui/textarea.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import { cn } from "../../lib"
+import { cn } from "../../lib/index"
 
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
