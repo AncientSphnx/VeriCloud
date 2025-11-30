0e44a6a3905ff7f5688f3c6c555a1b7dc1477f69
AncientSphnx <inuman739@gmail.com>
2025-11-30 07:58:11 +0530

    Trigger new deployment - cache fix

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,5 @@
-import { type ClassValue, clsx } from "clsx";
+// Utility functions for VeriCloud frontend
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge";
 
 /**
